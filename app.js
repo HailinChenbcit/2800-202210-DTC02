a90d373285eac82af11c6b532ee8f4095dc8cb88
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-05-03 18:04:24 -0700

    Implemented server on app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`App listening on port 3000.`);
+const port = process.env.port || 8000;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
 });

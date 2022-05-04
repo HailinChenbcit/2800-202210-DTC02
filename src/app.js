a4dafc22356e47ae63edc01c65dda0577e022cbe
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-05-04 16:43:49 -0700

    Added comments in app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+// Require libraries
 require("dotenv").config();
 
 const express = require("express");
@@ -8,18 +9,23 @@ const db = require("../config/database");
 
 const app = express();
 
+// Route for static public folder
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
 
+// Routes for ejs views regarding logging in
 app.get("/auth/login", authController.loginPage);
 app.post("/auth/register", authController.registerUser);
 app.post("/auth/login", authController.loginUser);
 
+
+// Routes for ejs views
 app.get("/home", indexController.homePage);
 app.get("/accounts", indexController.accountsPage)
 
+// Starts the server and the MongoDB database
 db.connect("mongodb://localhost:27017")
   .then(() =>
     app.listen(3000, () => {

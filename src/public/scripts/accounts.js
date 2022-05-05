db68cabe9fef3275e5f33b5219e186e9a4cefc73
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-05-05 15:11:38 -0700

    Created functions to initialize button functionalities - accounts.js

--- src/public/scripts/accounts.js
+++ src/public/scripts/accounts.js
@@ -21,6 +21,12 @@ const userArr = [
     {
         firstname: "Fonse",
         lastname: "Clarito",
+        email: "bill@eggonomics.tv",
+        admin: false
+    },
+    {
+        firstname: "Fonse",
+        lastname: "Clarito",
         email: "johndoe@my.bcit.ca",
         admin: false
     },
@@ -56,6 +62,31 @@ const userArr = [
     }
 ]
 
+function initResetBtn(btnID) {
+    document.querySelector(`#${btnID}`).addEventListener("touchend", () => {
+        console.log(`pushed ${btnID}`)
+    });
+}
+
+function initDelBtn(btnID) {
+    document.querySelector(`#${btnID}`).addEventListener("touchend", () => {
+        console.log(`pushed ${btnID}`)
+    });
+}
+
+function initDotMenuBtn(btnID, container) {
+    document.querySelector(`#${btnID}`).addEventListener("touchend", () => {
+        console.log(`pushed ${btnID}`)
+        console.log(container.getElementsByClassName("list-group-item"));
+    });
+}
+
+function initSlider(btnID) {
+    document.querySelector(`#${btnID}`).addEventListener("touchend", () => {
+        console.log(`pushed ${btnID}`)
+    });
+}
+
 function populatePage(users) {
     let users_div = document.querySelector("#accounts-target");
 
@@ -64,20 +95,20 @@ function populatePage(users) {
         let email_sect = `<span class="email">${user.email}</span>`;
         let admin_sect = ``;
         if (user.admin) {
-            admin_sect = `<span class="badge bg-secondary h-50">Admin</span>`;
+            admin_sect = `<span class="badge bg-secondary">Admin</span>`;
         }
 
-        let optionsbtn_sect = `<button type="button" class="btn" id="${i}" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="false" aria-controls="collapseExample">
+        let optionsbtn_sect = `<button type="button" class="btn" id="dotted${i}" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="false" aria-controls="collapseExample">
                                 <span class="material-symbols-outlined">more_vert</span>
                                </button>`;
         
         let options_sect = `<div class="collapse" id="collapse${i}">
                                 <div class="form-check form-switch">
-                                  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-                                  <label class="form-check-label" for="flexSwitchCheckDefault">Admin Privileges</label>
+                                  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault${i}">
+                                  <label class="form-check-label" for="flexSwitchCheckDefault${i}">Admin Privileges</label>
                                 </div>
-                                <button type="button" class="btn btn-reset">Reset Password</button>
-                                <button type="button" class="btn btn-del">
+                                <button type="button" class="btn btn-reset" id="reset${i}">Reset Password</button>
+                                <button type="button" class="btn btn-del" id="del${i}">
                                     <span class="material-symbols-outlined">delete</span>
                                 </button>
                             </div>`;
@@ -87,6 +118,13 @@ function populatePage(users) {
         single_cell.innerHTML = `<div class="singlecell">${email_sect + admin_sect + optionsbtn_sect + options_sect}</div>`;
 
         users_div.appendChild(single_cell);
+        console.log(single_cell.querySelector(`#reset${i}`));
+
+        initResetBtn(`reset${i}`);
+        initDelBtn(`del${i}`);
+        initDotMenuBtn(`dotted${i}`, users_div);
+        initSlider(`flexSwitchCheckDefault${i}`);
+
         i++;
     })
 }

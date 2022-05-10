c121fcc5bb95e62297f49f5161022ce03da496b3
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-05-10 12:53:41 -0700

    Refactored code in accounts.js - accounts.js

--- src/public/scripts/accounts.js
+++ src/public/scripts/accounts.js
@@ -1,67 +1,3 @@
-// Sample Database
-const userArr = [
-    {
-        firstname: "Fonse",
-        lastname: "Clarito",
-        email: "jalfonsclarito@gmail.com",
-        admin: true
-    },
-    {
-        firstname: "Fonse",
-        lastname: "Clarito",
-        email: "jimmy123@gmail.com",
-        admin: true
-    },
-    {
-        firstname: "Fonse",
-        lastname: "Clarito",
-        email: "phil_dunphy@hotmail.com",
-        admin: false
-    },
-    {
-        firstname: "Fonse",
-        lastname: "Clarito",
-        email: "bill@eggonomics.tv",
-        admin: false
-    },
-    {
-        firstname: "Fonse",
-        lastname: "Clarito",
-        email: "johndoe@my.bcit.ca",
-        admin: false
-    },
-    {
-        firstname: "Fonse",
-        lastname: "Clarito",
-        email: "jeremywang@google.ca",
-        admin: false
-    },
-    {
-        firstname: "Fonse",
-        lastname: "Clarito",
-        email: "lud_win@google.com",
-        admin: false
-    },
-    {
-        firstname: "Fonse",
-        lastname: "Clarito",
-        email: "jake_park21212121212121212121212121212@yahoo.com",
-        admin: true
-    },
-    {
-        firstname: "Fonse",
-        lastname: "Clarito",
-        email: "ramendestroyer1111111111111111111@canada.gov.ca",
-        admin: false
-    },
-    {
-        firstname: "Fonse",
-        lastname: "Clarito",
-        email: "aimer@google.jp",
-        admin: false
-    }
-]
-
 // Reset Password button listener
 function initResetBtn(btnID, user) {
     document.querySelector(`#${btnID}`).addEventListener("click", () => {
@@ -113,68 +49,19 @@ function initSlider(btnID, usercell, user) {
     });
 }
 
-function populatePage(users) {
-    let users_div = document.querySelector("#accounts-target");
-
-    let i = 0;
-    users.forEach((user) => {
-        let email_sect = `<span class="email">${user.email}</span>`;
-        let admin_sect = `<span class="bg-secondary admin-sect"></span>`;
-        let autochecked = "";
-        if (user.admin) {
-            admin_sect = `<span class="badge bg-secondary admin-sect">Admin</span>`;
-            autochecked = "checked";
-        }
-
-        let optionsbtn_sect = `<button type="button" class="btn" id="dotted${i}" data-bs-toggle="collapse" data-bs-target="#collapse${i}" aria-expanded="false" aria-controls="collapseExample">
-                                <span class="material-symbols-outlined">more_vert</span>
-                               </button>`;
-        
-        let options_sect = `<div class="collapse" id="collapse${i}">
-                                <div class="form-check form-switch" id="slidecheck${i}">
-                                  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault${i}" ${autochecked}>
-                                  <label class="form-check-label" for="flexSwitchCheckDefault${i}">Admin Privileges</label>
-                                </div>
-                                <button type="button" class="btn btn-reset" id="reset${i}">Reset Password</button>
-                                <button type="button" class="btn btn-del" id="del${i}">
-                                    <span class="material-symbols-outlined">delete</span>
-                                </button>
-                            </div>`;
-        
-        let single_cell = document.createElement("LI");
-        single_cell.classList.add("list-group-item");
-        single_cell.innerHTML = `<div class="singlecell">${email_sect + admin_sect + optionsbtn_sect + options_sect}</div>`;
-
-        users_div.appendChild(single_cell);
-
-        initResetBtn(`reset${i}`, user);
-        initDelBtn(`del${i}`, single_cell, users_div, user);
-        initSlider(`slidecheck${i}`, single_cell, user);
-        initDotMenuBtn(`dotted${i}`, users_div);
-
-        i++;
-    })
-}
-
+/**
+ * Sets up each user accounts event listeners.
+ */
 function setupUserOptions() {
     const accounts = document.querySelector("#accounts-target");
-    const accountsList = [...accounts.children]
-    // console.log(accountsList);
+    const accountsList = [...accounts.children];
 
-    accountsList.forEach((user, i) => {
-        console.log(user);
-        // initResetBtn(`reset${i}`, user);
-        // initDelBtn(`del${i}`, single_cell, users_div, user);
-        // initSlider(`slidecheck${i}`, single_cell, user);
+    accountsList.forEach((userElement, i) => {
         initDotMenuBtn(`dotted${i}`, accounts);
     })
 }
 
 function setup() {
-    // fetchUsers(); Get users from MongoDB database
-    // populatePage(userArr);
-    // initDotMenuBtn(`dotted1`, document.querySelector("#accounts-target"))
-    // console.log();
     setupUserOptions();
 }
 

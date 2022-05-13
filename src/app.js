9c349d60d6f2e50fa27a4e6eb67bc29fc69a97d7
hailin chen <hchen256@my.bcit.ca>
2022-05-12 19:43:32 -0700

    Updated env Port

--- src/app.js
+++ src/app.js
@@ -58,6 +58,6 @@ app.get("/home", ensureAuthenticated, indexController.homePage);
 app.get("/accounts", ensureAuthenticated, isAdmin, indexController.accountsPage);
 
 // Starts the server
-app.listen(3000, () =>
+app.listen(process.env.PORT || 3000, () =>
   console.log(`App listening on port 3000. | ${__dirname}!`)
 );

7b243778f978b814bac190813a7979a08bc8275c
boscochw_ubuntU_ideapad <bosco.chw@gmail.com>
2022-05-13 11:55:31 -0700

    add favicon and change page titles

--- src/public/scripts/dailyview.js
+++ src/public/scripts/dailyview.js
@@ -1,31 +1,27 @@
 async function deleteEntry() {
-    entry_id = (this.id).slice(11)
-    await $.ajax({
-        url: `http://localhost:3000/dailyView/remove/${entry_id}`,
-        type: "get",
-        success: (e) => {
-            console.log(e)
-        }
-    })
-    this.parentElement.parentElement.removeChild(this.parentElement);
-
+  entry_id = this.id.slice(11);
+  await $.ajax({
+    url: `http://localhost:3000/dailyView/remove/${entry_id}`,
+    type: "get",
+    success: (e) => {},
+  });
+  console.log(this);
+  this.parentElement.parentElement.removeChild(this.parentElement);
 }
 
 async function saveEntry() {
-    entry_id = (this.id).slice(4)
-    await $.ajax({
-        url: `http://localhost:3000/dailyView/update/${entry_id}`,
-        type: "get",
-        success: (e) => {
-            console.log(e)
-        }
-    })
-    // refresh div
-
+  entry_id = this.id.slice(4);
+  await $.ajax({
+    url: `http://localhost:3000/dailyView/update/${entry_id}`,
+    type: "get",
+    success: (e) => {
+      console.log(e);
+    },
+  });
+  // refresh div
 }
 
 $(document).ready(function () {
-    $("body").on('click', '.deleteEntry', deleteEntry)
-    $("body").on('click', '.saveEntry', saveEntry)
-
-})
+  $("body").on("click", ".deleteEntry", deleteEntry);
+  $("body").on("click", ".saveEntry", saveEntry);
+});

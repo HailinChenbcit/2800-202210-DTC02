a091459631a241fff41b7dd566e71b06d82838f5
Bosco_Acer <bosco.chw@gmail.com>
2022-05-18 09:20:49 -0700

    change start time to a datetime object in the createWorryTime function

--- src/controllers/worryTimeController.js
+++ src/controllers/worryTimeController.js
@@ -1,28 +1,35 @@
 const WorryTime = require("../models/WorryTime");
 
-const worryEntryController = {
-    duringWorryTimePage: (req, res) => {
-        WorryTime.find({user: req.session.passport.user}, (err, resp) => {
-            res.render("duringWorryTime", {"worryTime": resp})
-        })
-    },
-    createWorryTime: async (req, res) => {
-        const {startTime, duration, notificationEnabled, worries} = req.body;
-        const worryTime = WorryTime({
-            startTime,
-            duration,
-            notificationEnabled,
-            worries,
-            owner: req.user._id
-        })
+const offsetDate = (date, offset) =>
+  new Date(date.getTime() + offset * 60 * 1000);
 
-        try {
-            const newWorryTime = await worryTime.save();
-        } catch(e) {
-            console.log(e)
-        }
-        res.redirect("/home");
+const worryEntryController = {
+  duringWorryTimePage: (req, res) => {
+    WorryTime.find({ user: req.session.passport.user }, (err, resp) => {
+      res.render("duringWorryTime", { worryTime: resp });
+    });
+  },
+  createWorryTime: async (req, res) => {
+    const { startTime, duration, notificationEnabled, worries } = req.body;
+    const startTimeObj = offsetDate(
+      new Date(startTime),
+      req.session.timezoneOffset
+    );
+    const worryTime = WorryTime({
+      startTime: startTimeObj,
+      duration,
+      notificationEnabled,
+      worries,
+      owner: req.user._id,
+    });
+
+    try {
+      const newWorryTime = await worryTime.save();
+    } catch (e) {
+      console.log(e);
     }
-}
+    res.redirect("/home");
+  },
+};
 
 module.exports = worryEntryController;

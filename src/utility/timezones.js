34227269a17da28303a533c598ebe5da51bbbd45
Jose Alfonso Clarito <jalfonsclarito@gmail.com>
2022-05-19 11:38:06 -0700

    Documented formatting utility functions - timezones.js

--- src/utility/timezones.js
+++ src/utility/timezones.js
@@ -7,11 +7,21 @@
 const offsetDate = (date, offset) =>
     new Date(date.getTime() + offset * 60 * 1000);
 
+/**
+ * Formats the given rawDatetime into a String for display.
+ * @param {Date} rawDatetime a Date object
+ * @returns a String
+ */
 const formatToString = rawDatetime => rawDatetime.toLocaleString("en-GB", {
     dateStyle: "medium",
     timeStyle: "medium",
 })
 
+/**
+ * Formats the given rawDatetime into a URL String for routes.
+ * @param {Date} rawDatetime a Date object
+ * @returns a String
+ */
 const formatToURLString = rawDatetime =>
     `${String(rawDatetime.getFullYear()).padStart(4, "0")}${String(rawDatetime.getMonth()).padStart(2, "0")}${String(rawDatetime.getDate()).padStart(2, "0")}`
 
